8bc6551239e2c4beb7cef54fb157db10e9a9b20b
Linus Ng <linus.ng@cantor.com>
2025-11-17 16:36:33 +0000

    telegram introduction

--- run_auto_booker.py
+++ run_auto_booker.py
@@ -6,7 +6,7 @@ def run_courtside_booker():
     # venue_name = 'Bethnal Green Gardens'
     # venue_name = 'Ropemakers Field'
     venue_name = 'Victoria Park'
-    booking_date = '2025-11-17'
+    booking_date = '2025-11-18'
     venue_name = coverage.get(venue_name).get('website_name')
     courtside_booker(venue_name, booking_date)
 
